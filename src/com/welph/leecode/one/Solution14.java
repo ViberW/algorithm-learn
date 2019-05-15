2866a9dca1258277c88ea6e8aad65c2af1fad8a5
Veelur <wenbiao.wu@dadaabc.net>
2019-05-15 15:20:37 +0800

    14算法解法

---
+++ src/com/welph/leecode/one/Solution14.java
@@ -0,0 +1,41 @@
+package com.welph.leecode.one;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共 [前缀]。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * @author: Admin
+ * @date: 2019/5/15
+ * @Description: {相关描述}
+ */
+public class Solution14 {
+    public static void main(String[] args) {
+        String[] strs = new String[]{"", "b"};
+        System.out.println(longestCommonPrefix(strs));
+        System.out.println("".substring(0, 0));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        //从最小长度中找起  因为是寻找前缀 所以归为简单
+        if (null == strs || strs.length == 0) {
+            return "";
+        }
+        String minArr = strs[0];
+        for (String str : strs) {
+            if (minArr.length() > str.length()) {
+                minArr = str;
+            }
+        }
+        int i = 0;
+        lab:
+        for (; i < minArr.length(); i++) {
+            for (String str : strs) {
+                if (minArr.charAt(i) != str.charAt(i)) {
+                    break lab;
+                }
+            }
+        }
+        return minArr.substring(0, i);
+    }
+}
