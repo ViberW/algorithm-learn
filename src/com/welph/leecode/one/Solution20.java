51e36d914b0cb5a17817f24ef44edbce4b26be4a
Veelur <wenbiao.wu@dadaabc.net>
2019-05-16 15:38:44 +0800

    20算法解法

---
+++ src/com/welph/leecode/one/Solution20.java
@@ -0,0 +1,41 @@
+package com.welph.leecode.one;
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * @author: Admin
+ * @date: 2019/5/16
+ * @Description: {相关描述}
+ */
+public class Solution20 {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("([)]"));
+        System.out.println(isValid("()[]{}"));
+    }
+
+    public static boolean isValid(String s) {
+        char[] right = new char[]{')', '}', ']'};
+        Stack<Integer> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(0);
+            } else if (ch == '{') {
+                stack.push(1);
+            } else if (ch == '[') {
+                stack.push(2);
+            } else {
+                if (stack.isEmpty() || ch != right[stack.pop()]) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
