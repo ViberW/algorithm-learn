0289631a2a45facca099f2d507a7b89a3617c48c
eipers <15779229872@163.com>
2019-11-07 18:36:39 +0800

    49题解法

---
+++ src/com/welph/leecode/part_41_60/Solution49.java
@@ -0,0 +1,48 @@
+package com.welph.leecode.part_41_60;
+
+import java.util.*;
+
+/**
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * 输出:
+ * [
+ * ["ate","eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ * 说明：
+ * <p>
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ */
+public class Solution49 {
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        //需要找出快速判断两个字符串是否是异位词。
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (map.containsKey(key)) {
+                map.get(key).add(s);
+            } else {
+                map.put(key, new ArrayList<String>() {{
+                    add(s);
+                }});
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+
+}
