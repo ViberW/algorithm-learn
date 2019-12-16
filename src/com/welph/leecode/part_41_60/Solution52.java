b8955ad1e6bf1aabf8cd9f9858f8403e09c8036e
eipers <15779229872@163.com>
2019-12-16 15:30:12 +0800

    54,55解法

--- src/com/welph/leecode/part_41_60/Solution52.java
+++ src/com/welph/leecode/part_41_60/Solution52.java
@@ -2,6 +2,11 @@ package com.welph.leecode.part_41_60;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * <p>
+ * 给定一个整数 n，返回 n 皇后不同的解决方案的数量。
+ */
 public class Solution52 {
 
     public static void main(String[] args) {
