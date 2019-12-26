1dcb48c26b3d562eb686a71cb8da011bc7109e77
eipers <15779229872@163.com>
2019-12-26 18:33:34 +0800

    63解法

---
+++ src/com/welph/leecode/part_61_80/Solution63.java
@@ -0,0 +1,56 @@
+package com.welph.leecode.part_61_80;
+
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * <p>
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * <p>
+ * <p>
+ * <p>
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * <p>
+ * 说明：m 和 n 的值均不超过 100。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * [
+ *   [0,0,0],
+ *   [0,1,0],
+ *   [0,0,0]
+ * ]
+ * 输出: 2
+ * 解释:
+ */
+public class Solution63 {
+
+    public static void main(String[] args) {
+        int[][] obstacleGrid = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        System.out.println(uniquePathsWithObstacles(obstacleGrid));
+    }
+
+    /**
+     * 由于需要额外内存空间，则内存消耗，但书都快，递归可能内存消耗少，但数据可能欠缺
+     *
+     * @param obstacleGrid
+     * @return
+     */
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] arr = new int[m + 1][n + 1];
+        arr[1][0] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (obstacleGrid[i - 1][j - 1] != 0) {
+                    continue;
+                }
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+            }
+        }
+        return arr[m][n];
+    }
+
+}
