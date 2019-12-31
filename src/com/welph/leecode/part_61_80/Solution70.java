d790141d9380f2573686a0beaa5d41b02126ae03
eipers <15779229872@163.com>
2019-12-31 10:22:16 +0800

    70解法

---
+++ src/com/welph/leecode/part_61_80/Solution70.java
@@ -0,0 +1,63 @@
+package com.welph.leecode.part_61_80;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * <p>
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * <p>
+ * 注意：给定 n 是一个正整数。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ * <p>
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ */
+public class Solution70 {
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(3));
+        System.out.println(climbStairs1(3));
+    }
+
+    public static int climbStairs(int n) {
+        //递归，但这里有重复的计算
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        //考虑去重复
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public static int climbStairs1(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int first = 1;
+        int second = 2;
+        int result = 0;
+        for (int i = 3; i <= n; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+
+}
