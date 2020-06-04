9c8e827ea5f761aa61849d639c15e234b7b26442
eipers <15779229872@163.com>
2020-06-04 18:01:32 +0800

    sort demo with n to 2

--- src/com/welph/leecode/algorithm/sort/n_to_2/InsertSort.java
+++ src/com/welph/leecode/algorithm/sort/n_to_2/InsertSort.java
@@ -1,10 +1,40 @@
 package com.welph.leecode.algorithm.sort.n_to_2;
 
+import java.util.Arrays;
+
 /**
- * 插入排序
+ * 插入排序:
+ * 通过区分已排序和未排序区间, 对未排序区间的数据选择放置合适的已排序的位置;
+ * <p>
+ * 算法步骤:
+ * 1.将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+ * .
+ * 2.从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。
+ * . （如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
  */
 public class InsertSort {
     public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 3, 2, 1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public static void sort(int[] arr) {
+        int len = arr.length;
+        if (len <= 1) {
+            return;
+        }
+        for (int i = 1; i < len; i++) {
+            int val = arr[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {// 找到合适的位置
+                if (arr[j] > val) {
+                    arr[j + 1] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            arr[j + 1] = val;
+        }
     }
 }
--- src/com/welph/leecode/algorithm/sort/n_to_2/SelectionSort.java
+++ src/com/welph/leecode/algorithm/sort/n_to_2/SelectionSort.java
@@ -1,10 +1,41 @@
 package com.welph.leecode.algorithm.sort.n_to_2;
 
+import java.util.Arrays;
+
 /**
- * 选择排序
+ * 选择排序:
+ * 从未排序序列中找到合适大小的数据交换, 时间复杂度始终是O(n^2)
+ * <p>
+ * 算法步骤:
+ * 1.首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
+ * .
+ * 2.再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * .
+ * 3.重复第二步，直到所有元素均排序完毕。
  */
 public class SelectionSort {
     public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 3, 2, 1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public static void sort(int[] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            int min = i;
+            //找到小于当前位置的最小值
+            for (int j = i + 1; j < len; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            //若不为当前,则进行交换;
+            if (min != i) {
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
     }
 }
