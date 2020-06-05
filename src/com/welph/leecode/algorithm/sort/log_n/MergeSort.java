82759fe5476632b90a2074227ffc2d8226b8f9ea
eipers <15779229872@163.com>
2020-06-05 11:34:15 +0800

    shell and merge sort

--- src/com/welph/leecode/algorithm/sort/log_n/MergeSort.java
+++ src/com/welph/leecode/algorithm/sort/log_n/MergeSort.java
@@ -1,10 +1,69 @@
 package com.welph.leecode.algorithm.sort.log_n;
 
+import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
- * 归并排序
+ * 归并排序 : 分治思想
+ * 需要额外的内存空间
+ * 利用分治,使得比较两段数组的均是已经有序的. 只需要按照next节点比较大小合并即可;
+ * <p>
+ * 算法逻辑:
+ * 1.申请空间，使其大小为两个已经排序序列之和，该空间用来存放合并后的序列；
+ * .
+ * 2.设定两个指针，最初位置分别为两个已经排序序列的起始位置；
+ * .
+ * 3.比较两个指针所指向的元素，选择相对小的元素放入到合并空间，并移动指针到下一位置；
+ * .
+ * 4.重复步骤 3 直到某一指针达到序列尾；
+ * .
+ * 5.将另一序列剩下的所有元素直接复制到合并序列尾。
  */
 public class MergeSort {
     public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 3, 2, 1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public static void sort(int[] arr) {
+        sort(arr, 0, arr.length);
+    }
+
+    static void sort(int[] arr, int l, int r) {
+        if (r - l < 2) {
+            return;
+        }
+        int middle = (l + r) / 2;
+        sort(arr, l, middle);
+        sort(arr, middle, r);
+        merge(arr, l, middle, r);
+    }
+
+    static void merge(int[] arr, int l, int middle, int r) {
+        if (r - l < 2) {
+            return;
+        }
+        int originLeft = l;
+        //额外的空间内存
+        int[] result = new int[r - l];
+        int i = 0;
+        int mr = middle;
+        while (l < middle && mr < r) {
+            if (arr[l] <= arr[mr]) {
+                result[i] = arr[l++];
+            } else {
+                result[i] = arr[mr++];
+            }
+            i++;
+        }
+        for (; l < middle; l++, i++) {
+            result[i] = arr[l];
+        }
+        for (; mr < r; mr++, i++) {
+            result[i] = arr[mr];
+        }
+        //覆盖数据到arr
+        System.arraycopy(result, 0, arr, originLeft, result.length);
     }
 }
---
+++ src/com/welph/leecode/algorithm/sort/log_n/ShellSort.java
@@ -0,0 +1,58 @@
+package com.welph.leecode.algorithm.sort.log_n;
+
+import java.util.Arrays;
+
+/**
+ * 希尔排序:
+ * .  也称为递减增量排序算法. 插入排序的改进版本(非稳定)
+ * 改进插入排序方案:
+ * 1. 插入排序在对几乎已经排好序的数据操作时，效率高，即可以达到线性排序的效率；
+ * 2. 但插入排序一般来说是低效的，因为插入排序每次只能将数据移动一位；
+ * <p>
+ * 希尔排序思路: 先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，
+ * >            待整个序列中的记录“基本有序”时，再对全体记录进行依次直接插入排序
+ * <p>
+ * 算法步骤:
+ * 1.选择一个增量序列 t1，t2，……，tk，其中 ti > tj, tk = 1；
+ * .
+ * 2.按增量序列个数 k，对序列进行 k 趟排序；
+ * .
+ * 3.每趟排序，根据对应的增量 ti，将待排序列分割成若干长度为 m 的子序列，
+ * > 分别对各子表进行直接插入排序。仅增量因子为 1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 3, 2, 1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int[] arr) {
+        //选择一个增量序列 t1，t2，……，tk，其中 ti > tj, tk = 1；
+        int len = arr.length;
+        //动态规划增量
+        int gap = 1;
+        while (gap < len / 3) {
+            gap = gap * 3 + 1;
+        }
+        //按增量序列个数 k，对序列进行 k 趟排序；
+        while (gap > 0) {
+            System.out.println(gap);
+            //每趟排序，根据对应的增量 ti，将待排序列分割成若干长度为 m 的子序列，
+            for (int i = gap; i < arr.length; i++) {
+                //插入排序的逻辑
+                // 分别对各子表进行直接插入排序。仅增量因子为 1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+                {
+                    int val = arr[i];
+                    int j = i - gap;
+                    while (j >= 0 && arr[j] > val) {
+                        arr[j + gap] = arr[j];
+                        j -= gap;
+                    }
+                    arr[j + gap] = val;
+                }
+            }
+            gap = (int) Math.floor(gap / 3);
+        }
+    }
+}
