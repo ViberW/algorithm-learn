455e293bce0df79b2811ab069757c63e786c71f7
eipers <15779229872@163.com>
2020-06-08 10:12:24 +0800

    binary query 变种

---
+++ src/com/welph/leecode/algorithm/binaryquery/BinaryQuery.java
@@ -0,0 +1,99 @@
+package com.welph.leecode.algorithm.binaryquery;
+
+/**
+ * 二分查找 `变种`
+ */
+public class BinaryQuery {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 3, 3, 3, 5, 6};
+        System.out.println(firstEqual(arr, 3));
+        System.out.println(lastEqual(arr, 3));
+        System.out.println(firstGreate(arr, 4));
+        System.out.println(lastGreate(arr, 2));
+    }
+
+    //第一个相等
+    public static int firstEqual(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length - 1;
+        int mid;
+        int index = -1;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (arr[mid] > target) {
+                r = mid - 1;
+            } else if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                //因为是找到第一个相等的,所以尝试往前找
+                index = mid;
+                r = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    //第一个相最后一个相等
+    public static int lastEqual(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length - 1;
+        int mid;
+        int index = -1;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (arr[mid] > target) {
+                r = mid - 1;
+            } else if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                //因为是找到最后一个相等的,所以尝试往前找
+                index = mid;
+                l = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    //第一个大于等于
+    public static int firstGreate(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length - 1;
+        int mid;
+        int index = -1;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (arr[mid] > target) {
+                index = mid;
+                r = mid - 1;
+            } else if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                index = mid;
+                r = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    //最后一个大于等于
+    public static int lastGreate(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length - 1;
+        int mid;
+        int index = -1;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (arr[mid] > target) {
+                index = mid;
+                r = mid - 1;
+            } else if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                index = mid;
+                l = mid + 1;
+            }
+        }
+        return index;
+    }
+}
