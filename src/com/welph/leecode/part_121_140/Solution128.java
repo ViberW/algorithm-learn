a48e38ec9af4019220165f9c4a753d9934b587d5
viber <15779229872@163.com>
2020-12-15 17:35:59 +0800

    solution128&130题解

--- src/com/welph/leecode/part_121_140/Solution128.java
+++ src/com/welph/leecode/part_121_140/Solution128.java
@@ -1,68 +1,66 @@
 package com.welph.leecode.part_121_140;
 
-import com.welph.leecode.common.TreeNode;
+import java.util.*;
 
 /**
- * .给定一个二叉树，它的每个结点都存放一个0-9的数字，每条从根到叶子节点的路径都代表一个数字。
- * .例如，从根到叶子节点路径 1->2->3 代表数字 123。
- * .计算从根到叶子节点生成的所有数字之和。
- * .说明:叶子节点是指没有子节点的节点。
- * .示例 1:
- * .
- * .输入: [1,2,3]
- * .    1
- * .   / \
- * .  2   3
- * .输出: 25
- * .解释:
- * .从根到叶子节点路径 1->2 代表数字 12.
- * .从根到叶子节点路径 1->3 代表数字 13.
- * .因此，数字总和 = 12 + 13 = 25.
- * .示例 2:
- * .输入: [4,9,0,5,1]
- * .    4
- * .   / \
- * .  9   0
- * . / \
- * .5   1
- * .输出: 1026
- * .解释:
- * .从根到叶子节点路径 4->9->5 代表数字 495.
- * .从根到叶子节点路径 4->9->1 代表数字 491.
- * .从根到叶子节点路径 4->0 代表数字 40.
- * .因此，数字总和 = 495 + 491 + 40 = 1026.
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * 进阶：你可以设计并实现时间复杂度为O(n) 的解决方案吗？
+ * 示例 1：
+ * <p>
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
  */
 public class Solution128 {
 
     public static void main(String[] args) {
-        TreeNode testData = TreeNode.createTestData("[]");
-
-        System.out.println(sumNumbers(testData));
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println(longestConsecutive(nums));
+        int[] nums2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        System.out.println(longestConsecutive(nums2));
+        int[] nums3 = {-7, -1, 3, -9, -4, 7, -3, 2, 4, 9, 4, -9, 8, -7, 5, -1, -7};
+        System.out.println(longestConsecutive(nums3));
+        int[] nums4 = {-2, -3, -3, 7, -3, 0, 5, 0, -8, -4, -1, 2};
+        System.out.println(longestConsecutive(nums4));
     }
 
     /**
-     * 深度优先搜索就可以了
+     * 并查集
+     * --每次想办法进行合并
      */
-    public static int sumNumbers(TreeNode root) {
-        if (root == null) {
-            return 0;
+    public static int longestConsecutive(int[] nums) {
+        //每次查询num-1和num+1是否存在,存在则保证信息
+        int max = 0;
+        TreeMap<Integer, Integer> tree = new TreeMap<>();
+        Map.Entry<Integer, Integer> entry;
+        int cur;
+        for (int num : nums) {
+            entry = tree.ceilingEntry(num + 1);
+            cur = num;
+            if (entry != null && entry.getKey() == num + 1) {
+                tree.remove(entry.getKey());
+                cur = entry.getValue();
+            }
+            entry = tree.floorEntry(num);
+            if (entry == null || entry.getValue() < num - 1) {
+                tree.put(num, cur);
+                max = Math.max(max, cur - num + 1);
+            } else {
+                cur = Math.max(entry.getValue(), cur);
+                tree.put(entry.getKey(), cur);
+                max = Math.max(max, cur - entry.getKey() + 1);
+            }
         }
-        return sumNumbers(root, 0);
+        return max;
     }
 
-    public static int sumNumbers(TreeNode root, int val) {
-        int total = 0;
-        if (root.left == null && root.right == null) {
-            //叶子节点
-            total += val + root.val;
-        } else {
-            if (root.left != null) {
-                total += sumNumbers(root.left, (val + root.val) * 10);
-            }
-            if (root.right != null) {
-                total += sumNumbers(root.right, (val + root.val) * 10);
-            }
-        }
-        return total;
-    }
 }
+
---
+++ src/com/welph/leecode/part_121_140/Solution130.java
@@ -0,0 +1,68 @@
+package com.welph.leecode.part_121_140;
+
+import com.welph.leecode.common.TreeNode;
+
+/**
+ * .给定一个二叉树，它的每个结点都存放一个0-9的数字，每条从根到叶子节点的路径都代表一个数字。
+ * .例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ * .计算从根到叶子节点生成的所有数字之和。
+ * .说明:叶子节点是指没有子节点的节点。
+ * .示例 1:
+ * .
+ * .输入: [1,2,3]
+ * .    1
+ * .   / \
+ * .  2   3
+ * .输出: 25
+ * .解释:
+ * .从根到叶子节点路径 1->2 代表数字 12.
+ * .从根到叶子节点路径 1->3 代表数字 13.
+ * .因此，数字总和 = 12 + 13 = 25.
+ * .示例 2:
+ * .输入: [4,9,0,5,1]
+ * .    4
+ * .   / \
+ * .  9   0
+ * . / \
+ * .5   1
+ * .输出: 1026
+ * .解释:
+ * .从根到叶子节点路径 4->9->5 代表数字 495.
+ * .从根到叶子节点路径 4->9->1 代表数字 491.
+ * .从根到叶子节点路径 4->0 代表数字 40.
+ * .因此，数字总和 = 495 + 491 + 40 = 1026.
+ */
+public class Solution130 {
+
+    public static void main(String[] args) {
+        TreeNode testData = TreeNode.createTestData("[]");
+
+        System.out.println(sumNumbers(testData));
+    }
+
+    /**
+     * 深度优先搜索就可以了
+     */
+    public static int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return sumNumbers(root, 0);
+    }
+
+    public static int sumNumbers(TreeNode root, int val) {
+        int total = 0;
+        if (root.left == null && root.right == null) {
+            //叶子节点
+            total += val + root.val;
+        } else {
+            if (root.left != null) {
+                total += sumNumbers(root.left, (val + root.val) * 10);
+            }
+            if (root.right != null) {
+                total += sumNumbers(root.right, (val + root.val) * 10);
+            }
+        }
+        return total;
+    }
+}
