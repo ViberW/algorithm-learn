d5cd5eaf914f29a3d6ee2a4aaf53e62169fc3676
viber <15779229872@163.com>
2021-07-01 16:23:28 +0800

    Solution316-319题解

---
+++ src/com/welph/leecode/algorithm/z7z8/PrefixSumDemo.java
@@ -0,0 +1,25 @@
+package com.welph.leecode.algorithm.z7z8;
+
+/**
+ * @author Viber
+ * @version 1.0
+ * @apiNote 区间数据--前缀和
+ * @since 2021/7/1 13:38
+ */
+public class PrefixSumDemo {
+
+    public static void main(String[] args) {
+        String str = "abcdefg....xyz";
+    }
+
+    /**
+     * 环绕字符串中唯一的子字符串
+     * 字符串str中, 前后相连为[环形字符串]
+     * 找出str中有多少个唯一的p的非空子串匹配
+     */
+    public int findSubstringInWraproundString(String str, String p) {
+
+
+        return 0;
+    }
+}
---
+++ src/com/welph/leecode/part_301_320/Solution318.java
@@ -0,0 +1,56 @@
+package com.welph.leecode.part_301_320;
+
+/**
+ * 给定一个字符串数组words，找到length(word[i]) * length(word[j])的最大值，
+ * 并且这两个单词不含有公共字母。你可以认为每个单词只包含小写字母。如果不存在这样的两个单词，返回 0。
+ * <p>
+ * 示例1:
+ * 输入: ["abcw","baz","foo","bar","xtfn","abcdef"]
+ * 输出: 16
+ * 解释: 这两个单词为 "abcw", "xtfn"。
+ * <p>
+ * 示例 2:
+ * 输入: ["a","ab","abc","d","cd","bcd","abcd"]
+ * 输出: 4
+ * 解释: 这两个单词为 "ab", "cd"。
+ * <p>
+ * 示例 3:
+ * 输入: ["a","aa","aaa","aaaa"]
+ * 输出: 0
+ * 解释: 不存在这样的两个单词。
+ */
+public class Solution318 {
+
+    public static void main(String[] args) {
+        String[] words = {"abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
+        System.out.println(maxProduct(words));
+    }
+
+    /**
+     * 判断是否存在相同的字母, 26bit位
+     */
+    public static int maxProduct(String[] words) {
+        int len = words.length;
+        int max = 0;
+        int[] v = new int[len];
+        for (int i = 0; i < len; i++) {
+            v[i] = buildBit(words[i]);
+        }
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if ((v[i] & v[j]) == 0) {
+                    max = Math.max(max, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return max;
+    }
+
+    private static int buildBit(String word) {
+        int bit = 0;
+        for (int i = 0; i < word.length(); i++) {
+            bit |= (1 << (word.charAt(i) - 'A'));
+        }
+        return bit;
+    }
+}
