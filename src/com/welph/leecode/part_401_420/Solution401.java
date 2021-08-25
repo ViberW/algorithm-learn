97679b257d6b5309bed33b12a920528fd162f360
viber <15779229872@163.com>
2021-08-25 17:12:16 +0800

    Solution401题解

---
+++ src/com/welph/leecode/part_401_420/Solution401.java
@@ -0,0 +1,90 @@
+package com.welph.leecode.part_401_420;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 二进制手表顶部有 4 个 LED 代表 小时（0-11），底部的 6 个 LED 代表 分钟（0-59）。每个 LED 代表一个 0 或 1，最低位在右侧。
+ * 例如，下面的二进制手表读取 "3:25" 。
+ * <p>
+ * 给你一个整数 turnedOn ，表示当前亮着的 LED 的数量，返回二进制手表可以表示的所有可能时间。你可以 按任意顺序 返回答案。
+ * 小时不会以零开头：
+ * <p>
+ * 例如，"01:00" 是无效的时间，正确的写法应该是 "1:00" 。
+ * 分钟必须由两位数组成，可能会以零开头：
+ * <p>
+ * 例如，"10:2" 是无效的时间，正确的写法应该是 "10:02" 。
+ * <p>
+ * 示例 1：
+ * 输入：turnedOn = 1
+ * 输出：["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
+ * <p>
+ * 示例 2：
+ * 输入：turnedOn = 9
+ * 输出：[]
+ * <p>
+ * 提示：
+ * 0 <= turnedOn <= 10
+ */
+public class Solution401 {
+
+    public static void main(String[] args) {
+        System.out.println(readBinaryWatch(1));
+        System.out.println(readBinaryWatch(9));
+        System.out.println(readBinaryWatch(2));
+    }
+
+    /**
+     * 前4后6, 小时不会从0开始, 说明小时必须亮灯
+     */
+    public static List<String> readBinaryWatch(int turnedOn) {
+        List<String> ret = new ArrayList<>();
+        for (int i = 0; i <= 4 && i <= turnedOn; i++) {
+            List<Integer> hours = binary(i, 4, 11);
+            if (null == hours || hours.isEmpty()) {
+                continue;
+            }
+            List<Integer> minutes = binary(turnedOn - i, 6, 59);
+            if (null == minutes || minutes.isEmpty()) {
+                continue;
+            }
+            for (Integer hour : hours) {
+                for (Integer minute : minutes) {
+                    ret.add(new StringBuilder()
+                            .append(hour)
+                            .append(":")
+                            .append(minute < 10 ? "0" : "")
+                            .append(minute).toString());
+                }
+            }
+        }
+        return ret;
+    }
+
+    private static List<Integer> binary(int light, int limit, int max) {
+        if (light > limit) {
+            return null;
+        }
+        List<Integer> ret = new ArrayList<>();
+        back(ret, light, limit, max, 0, 0);
+        return ret;
+    }
+
+    static void back(List<Integer> ret, int reduce, int limit, int max,
+                     int val, int index) {
+        if (val > max) {
+            return;
+        }
+        if (reduce == 0) {
+            ret.add(val);
+            return;
+        }
+        reduce--;
+        for (; index < limit; index++) {
+            back(ret, reduce, limit, max, val + (1 << index), index + 1);
+        }
+    }
+}
