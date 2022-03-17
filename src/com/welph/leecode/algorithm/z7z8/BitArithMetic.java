100a8128c41aada43eae27780ca79f976669a938
viber <15779229872@163.com>
2022-03-17 15:28:31 +0800

    部分补充

--- src/com/welph/leecode/algorithm/z7z8/BitArithMetic.java
+++ src/com/welph/leecode/algorithm/z7z8/BitArithMetic.java
@@ -25,16 +25,28 @@ public class BitArithMetic {
         a = a ^ b;//a=(a^b)^(a^b^b)=0^b=0
     }
 
+    public static void main(String[] args) {
+        int a = 0b1010010101000;
+        tableSizeFor(a);
+        numberOfLeadingZeros(a);
+    }
+
     /**
      * 获取数值最接近的2的幂次方值
      */
     public static int tableSizeFor(int cap) {
         int n = cap - 1;
-        n |= n >>> 1;
-        n |= n >>> 2;
-        n |= n >>> 4;
-        n |= n >>> 8;
-        n |= n >>> 16;
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 1;//保证了第二位=1
+        System.out.println(Integer.toBinaryString(n) + "==========" +Integer.toBinaryString(n >>> 1));
+        n |= n >>> 2;//保证了第3,4位=1 因为上面已经把第一,二位都调整为1了  --此时已经1,2,3,4都为1
+        System.out.println(Integer.toBinaryString(n)+ "==========" +Integer.toBinaryString(n >>> 2));
+        n |= n >>> 4;//保证了5~8变为1, 此时1~8变为1
+        System.out.println(Integer.toBinaryString(n)+ "==========" +Integer.toBinaryString(n >>> 4));
+        n |= n >>> 8;//保证了9~16变为1, 此时1~16变为1
+        System.out.println(Integer.toBinaryString(n)+ "==========" +Integer.toBinaryString(n >>> 8));
+        n |= n >>> 16;//保证了17~31变为1, 此时1~31变为1
+        System.out.println(Integer.toBinaryString(n)+ "==========" +Integer.toBinaryString(n >>> 16));
         return (n < 0) ? 1 : (n >= Integer.MAX_VALUE) ? Integer.MAX_VALUE : n + 1;
     }
 
@@ -45,23 +57,23 @@ public class BitArithMetic {
         if (i == 0)
             return 32;
         int n = 1;
-        if (i >>> 16 == 0) {
+        if (i >>> 16 == 0) {//一半的位置, 若是等于0 说明至少有16个0, 有符号左位移
             n += 16;
             i <<= 16;
         }
-        if (i >>> 24 == 0) {
+        if (i >>> 24 == 0) {//以上类推
             n += 8;
             i <<= 8;
         }
-        if (i >>> 28 == 0) {
+        if (i >>> 28 == 0) {//以上类推
             n += 4;
             i <<= 4;
         }
-        if (i >>> 30 == 0) {
+        if (i >>> 30 == 0) {//以上类推
             n += 2;
             i <<= 2;
         }
-        n -= i >>> 31;
+        n -= i >>> 31;//是否31位存在0或者1
         return n;
     }
 
--- src/com/welph/leecode/part_1_500/part_361_380/Solution363.java
+++ src/com/welph/leecode/part_1_500/part_361_380/Solution363.java
@@ -1,5 +1,7 @@
 package com.welph.leecode.part_1_500.part_361_380;
 
+import java.util.TreeSet;
+
 /**
  * 给你一个 m x n 的矩阵 matrix 和一个整数 k ，找出并返回矩阵内部矩形区域的不超过 k 的最大数值和。
  * 题目数据保证总会存在一个数值和不超过 k 的矩形区域。
@@ -56,6 +58,31 @@ public class Solution363 {
             }
         }
         return max;
+    }
+
+    public int maxSumSubmatrix2(int[][] matrix, int k) {
+        int ans = Integer.MIN_VALUE;
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m; ++i) { // 枚举上边界
+            int[] sum = new int[n];
+            for (int j = i; j < m; ++j) { // 枚举下边界
+                for (int c = 0; c < n; ++c) {
+                    sum[c] += matrix[j][c]; // 更新每列的元素和
+                }
+                TreeSet<Integer> sumSet = new TreeSet<Integer>();
+                sumSet.add(0);
+                int s = 0;
+                for (int v : sum) {
+                    s += v;
+                    Integer ceil = sumSet.ceiling(s - k);
+                    if (ceil != null) {
+                        ans = Math.max(ans, s - ceil);
+                    }
+                    sumSet.add(s);
+                }
+            }
+        }
+        return ans;
     }
 
     /*for(int down=up;down<m;down++){
