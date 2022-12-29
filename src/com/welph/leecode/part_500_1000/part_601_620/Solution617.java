4b677c0ea94cc5ff973834555d26a1cfc33c8930
viber <15779229872@163.com>
2022-12-29 14:55:03 +0800

    Solution617题解

---
+++ src/com/welph/leecode/part_500_1000/part_601_620/Solution617.java
@@ -0,0 +1,43 @@
+package com.welph.leecode.part_500_1000.part_601_620;
+
+import com.welph.leecode.common.TreeNode;
+
+/**
+ * 给你两棵二叉树： root1 和 root2 。
+ * 想象一下，当你将其中一棵覆盖到另一棵之上时，两棵树上的一些节点将会重叠（而另一些不会）。
+ * 你需要将这两棵树合并成一棵新二叉树。合并的规则是：如果两个节点重叠，
+ * 那么将这两个节点的值相加作为合并后节点的新值；否则，不为 null 的节点将直接作为新二叉树的节点。
+ * 返回合并后的二叉树。
+ * 注意: 合并过程必须从两个树的根节点开始。
+ * <p>
+ * 示例 1：
+ * 输入：root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+ * 输出：[3,4,5,5,4,null,7]
+ * <p>
+ * 示例 2：
+ * 输入：root1 = [1], root2 = [1,2]
+ * 输出：[2,2]
+ * <p>
+ * 提示：
+ * 两棵树中的节点数目在范围 [0, 2000] 内
+ * -10^4 <= Node.val <= 10^4
+ */
+public class Solution617 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        } else if (root2 == null) {
+            return root1;
+        } else {
+            root1.val += root2.val;
+            root1.left = mergeTrees(root1.left, root2.left);
+            root1.right = mergeTrees(root1.right, root2.right);
+            return root1;
+        }
+    }
+}
