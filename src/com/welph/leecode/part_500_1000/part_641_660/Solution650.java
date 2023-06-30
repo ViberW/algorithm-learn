64480b2c98dfffa85bdc9ba1e5596fd93d7a6811
15779229872 <15779229872@163.com>
2023-06-30 17:50:56 +0800

    Solution649题解

---
+++ src/com/welph/leecode/part_500_1000/part_641_660/Solution650.java
@@ -0,0 +1,19 @@
+package com.welph.leecode.part_500_1000.part_641_660;
+
+/**
+ * @author Viber
+ * @version 1.0
+ * @apiNote
+ * @since 2023/6/30 17:50
+ */
+public class Solution650 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int minSteps(int n) {
+
+        return 0;
+    }
+}
